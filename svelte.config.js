6d859917b97144040183db4d3110a1dd612543f0
daniel r <daniel.ratmiroff@ultratendency.com>
2022-12-05 12:59:29 +0100

    vercel deploy action

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import preprocess from 'svelte-preprocess';
-
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-auto';
 
 export default {
 	preprocess: preprocess(),
